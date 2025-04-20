e34c2a99ca62bd2ee5b0755b16af9ca40efe6ddb
hugues_esc <hugues.escoffier@gmail.com>
2025-04-20 20:07:48 +0200

    add test conversion.py

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,21 +1,19 @@
 from ppm_to_conc import conversion
 
 
-def test_conv_ppm_to_g():
+def test_calculate_sequence_mw():
+    sequence_test = 'MAAVAAVAAR'
+    mw = conversion.calculate_sequence_mw(sequence_test)
+
+    assert isinstance(mw, float)
+    assert mw > 0
+
+
+def test_ppm_to_grams():
     ppm = 459 
     molecular_weight = 106874  # Dalton
 
     result = conversion.ppm_to_grams(ppm, molecular_weight)
 
-    # Check that the result is numeric and positive
     assert isinstance(result, float)
     assert result > 0
-
-
-def test_calculate_sequence_mw():
-    sequence_test = 'MAAVAAVAAR'
-    mw = conversion.calculate_sequence_mw(sequence_test)
-
-    # Check that the result is numeric and positive
-    assert isinstance(mw, float)
-    assert mw > 0
